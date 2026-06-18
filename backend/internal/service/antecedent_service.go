54e5018041462ee2f27eb2ba80d71861dec6fd0b
WilliamBreno <will500mkc@hotmail.com>
2026-06-18 13:31:40 -0300

    V2.4

--- backend/internal/service/antecedent_service.go
+++ backend/internal/service/antecedent_service.go
@@ -1,30 +1,29 @@
 package service
 
 import (
-	"rpg-manager/internal/domain"
-
-	"gorm.io/gorm"
+    "rpg-manager/internal/domain"
+    "gorm.io/gorm"
 )
 
 type AntecedentService struct {
-	db *gorm.DB
+    db *gorm.DB
 }
 
 func NewAntecedentService(db *gorm.DB) *AntecedentService {
-	return &AntecedentService{db: db}
+    return &AntecedentService{db: db}
 }
 
-func (s *AntecedentService) GetAll(edition string) ([]domain.Background, error) {
-	var backgrounds []domain.Background
-	result := s.db.Where("edition = ?", edition).Find(&backgrounds)
-	return backgrounds, result.Error
+func (s *AntecedentService) GetAll(edition string) ([]domain.Antecedent, error) {
+    var antecedents []domain.Antecedent
+    result := s.db.Where("edition = ?", edition).Find(&antecedents)
+    return antecedents, result.Error
 }
 
-func (s *AntecedentService) GetByID(id uint) (*domain.Background, error) {
-	var background domain.Background
-	result := s.db.First(&background, id)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return &background, nil
+func (s *AntecedentService) GetByID(id uint) (*domain.Antecedent, error) {
+    var antecedent domain.Antecedent
+    result := s.db.First(&antecedent, id)
+    if result.Error != nil {
+        return nil, result.Error
+    }
+    return &antecedent, nil
 }
